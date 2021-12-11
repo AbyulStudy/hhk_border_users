caed2bc0f607f99bb6bc4f27f459cc1eaa8aa1e8
Charlie-aws-hotfix <tchoi.tab@gmail.com>
2021-12-11 11:43:24 +0000

    backend_userinfo finished

--- back/controllers/users/userInfo.js
+++ back/controllers/users/userInfo.js
@@ -1,5 +1,5 @@
 const { user } = require('../../models');
-const { isAuthorized } = require('../tokenFunction')
+const { isAuthorized } = require('../tokenFunction');
 
 module.exports = (req, res) => {
 
