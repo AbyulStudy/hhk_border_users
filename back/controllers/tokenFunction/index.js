75579317dea957909d4cb165fbab78a93b40a89a
Charlie-aws-hotfix <tchoi.tab@gmail.com>
2021-12-11 11:46:24 +0000

    changed expiresIn to 3h

--- back/controllers/tokenFunction/index.js
+++ back/controllers/tokenFunction/index.js
@@ -5,12 +5,11 @@ module.exports = {
     // create AccessToken 
     generateAccessToken : (data) => {
         // jwt.sign(payload, secretOrPrivateKey, [options, callback])
-        return sign(data, process.env.ACCESS_SECRET, {expiresIn:"15s"});            
+        return sign(data, process.env.ACCESS_SECRET, {expiresIn:"3h"});            
     },
     sendAccessToken : (req, res, accessToken) => {
         
         res.cookie('hhkToken',accessToken,{ httpOnly:true, secure:true, sameSite:'None' });
-        //res.cookie('hhkToken',accessToken,{ httpOnly:true, secure:true, domain: '127.0.0.1', sameSite:'Lax' });
         //토큰을 보낸 뒤 index.html로 리다이렉트 합니다.
         let backURL=req.header('Referer') || '/';
         if( backURL.includes('github.io') ) backURL += 'simpleboard-test/';
